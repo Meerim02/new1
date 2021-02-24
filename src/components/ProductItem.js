4d7519e001e1c971f243b160b78c6f3cc3f33856
Your Name <you@example.com>
2021-02-24 19:05:32 +0600

    form

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,13 +2,15 @@ import React from 'react';
 import cardPhoto from '../img/image.jpg'
 
 const ProductItem = (props) => {
+  const item = props.item
     return (
-        <div className="card">
+        <div className="card mt-4">
         <img className="card-img-top" src={cardPhoto} alt="Card cap"/>
         <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
-          <button className="btn btn-primary">Go somewhere</button>
+          <h5 className="card-title">{item.title}</h5>
+          <p className="card-text">{item.description}</p>
+          <p className="card-text">{item.price}som</p>
+          <button onClick={props.addToCart} className="btn btn-primary">Go somewhere</button>
         </div>
       </div>
     );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ProductItem from './ProductItem';
+import Cart from "./Cart";
 
-const ProductList = () => {
-    return (
-        <div>
-            <ul className="row product-list">
-             <li className="col-3">
-              <ProductItem title="Asan"  description="sdfsdfsd"/>
-              </li> 
 
-              <li className="col-3">
-              <ProductItem title="Hasan"  description="dsfdsfdsf" />
-              </li>  
+class ProductList extends Component {
 
-              <li className="col-3">
-              <ProductItem title="Ali" description="descripdsfsdfsdftion"/>
-              </li>  
-              <li className="col-3">
-              <ProductItem title="Asi" description="descripsdfsdfdstion"/>
-              </li>  
-            </ul>
-        </div>
-     
-    );
-};
+    state = {
+        productList: [
+            { id: 35, title: "Asan", description: "It is a good product1",price:120,count:1 },
+            { id: 135, title: "Hasan", description: "It is a good product2" ,price:140,count:1 },
+            { id: 356, title: "Ali", description: "It is a good product3",price:160,count:1  },
+            { id: 329, title: "Asi", description: "It is a good product4",price:180,count:1  }
+
+        ],
+        cart: []
+
+    }
+
+
+    consolTitle = (title) => () => {
+        console.log(title);
+    }
+    addToCart = (id) => () => {
+
+        const exist = this.state.cart.find((item) => item.id === id)
+
+        if (exist === undefined) {
+            this.setState((state) => {
+                const item = state.productList.find((item) => item.id === id)
+                return {
+                    cart: [...state.cart, { ...item }]
+                }
+            })
+        }
+    }
+
+
+    render() {
+        const totalCart = this.state.cart.reduce((ans,item)=>item.price+ans,0)
+        return (
+            <div>
+                <ul className="row product-list">
+                    {this.state.productList.map((item) => (
+                        <li key={item.id} className="col-3">
+                            <ProductItem
+                               item={item}
+                                consolTitle={this.consolTitle(item.title)}
+                                addToCart={this.addToCart(item.id)}
+                            />
+                        </li>
+                    ))
+                    }
+                </ul>
+                <Cart totalCart={totalCart}  cart={this.state.cart}/>
+            </div>
+        );
+    }
+}
+console.log();
 
 export default ProductList;
